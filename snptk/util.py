abcaef1099642e4e0b1e64d383afa6df6e151a81
Kevin Counts <kevin.counts@epi.usf.edu>
2018-10-03 22:56:10 -0400

    Add date to debug prefix

--- snptk/util.py
+++ snptk/util.py
@@ -7,6 +7,6 @@ def debug(message, message_level=1):
     level = int(os.environ.get('DEBUG', 0))
 
     if level >= message_level:
-        timestamp = time.strftime('%H:%M:%S', time.gmtime(time.time()))
+        timestamp = time.strftime('%F %H:%M:%S', time.gmtime(time.time()))
         node = socket.gethostname()
         print(f'{timestamp} {node} DEBUG({message_level}): {message}', file=sys.stderr)
