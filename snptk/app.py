e41d59427cbeeff78230ae7f421fb23713797005
Leonardo Moreno <jmoreno3102@gmail.com>
2018-10-04 15:52:38 -0400

    Checkpoint

--- snptk/app.py
+++ snptk/app.py
@@ -6,41 +6,37 @@ from concurrent.futures import ProcessPoolExecutor
 import snptk.core
 import snptk.util
 
-# [ (snpid, update_snpid),
-#   (snpid, update_snpid),
-# ]
-
 def update_snpid_and_position(args):
     bim_fname = args['bim']
     dbsnp_fname = args['dbsnp']
     snp_history_fname = args['snp_history']
     rs_merge_fname = args['rs_merge']
 
-    snp_ids = set()
 
     bim_entries = snptk.core.load_bim(bim_fname)
     snp_history = snptk.core.execute_load(snptk.core.load_snp_history, snp_history_fname, merge_method='set')
     rs_merge = snptk.core.execute_load(snptk.core.load_rs_merge, rs_merge_fname, merge_method='update')
 
-    for entry in bim_entries:
-        snp_ids.add(entry['snp_id'])
-
     update_look_up = {}
-    deleted_snp_ids = []
-    updated_snp_ids = set(snp_ids)
-    for snp_id in snp_ids:
-        updated_snp_id = snptk.core.update_snp_id(snp_id, snp_history, rs_merge)
 
-        if updated_snp_id == 'deleted':
-            updated_snp_ids.remove(snp_id)
-            snptk.util.debug(f'SNP id deleted {snp_id}')
-            deleted_snp_ids.append(snp_id)
-        elif updated_snp_id == 'unchanged':
+    snp_ids = set()
+    snp_ids_deleted = set()
+
+    for entry in bim_entries:
+        snp_id = snptk.core.update_snp_id(entry['snp_id'], snp_history, rs_merge)
+
+        if snp_id == entry['snp_id']:
+            snp_ids.add(snp_id)
             continue
-        else:
-            snptk.util.debug(f'SNP id was {snp_id}, Now is {updated_snp_id}')
+
+        elif snp_id == None:
+            snp_ids_deleted.add(snp_id)
+            snptk.util.debug(f'SNP id deleted {snp_id}')
+
+        elif updated_snp_id != snp_id:
             update_look_up[snp_id] = updated_snp_id
             updated_snp_ids.add(updated_snp_id)
+            snptk.util.debug(f'SNP id was {snp_id}, Now is {updated_snp_id}')
 
     db = snptk.core.execute_load(snptk.core.load_dbsnp_by_snp_id, dbsnp_fname, updated_snp_ids, merge_method='update')
 
