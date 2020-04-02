61d609dbdcf39cbc3b47ec76bad7e663e29aadcf
Kevin Counts <counts@digicat.org>
2020-04-02 10:07:26 -0400

    Update tests/test_snptk_app.py with new structure
    - Renamed testcase to TestSnpTkAppUpdateLogic
    - Introduced namedtuple for expected output
    - Shortened and renamed cases to test_<description>

--- tests/test_snptk_app.py
+++ tests/test_snptk_app.py
@@ -1,5 +1,6 @@
 import unittest
 
+from collections import namedtuple
 from os.path import abspath, basename, dirname, join
 
 import snptk.app
@@ -9,56 +10,66 @@ BASE = abspath(join(dirname(__file__), '..'))
 def test_data(path):
     return join(abspath(dirname(__file__)), 'data', path)
 
-class TestSnpTkApp(unittest.TestCase):
+UpdateLogicOutput = namedtuple("UpdateLogicOutput", ["snps_del", "snps_up", "coords_up", "chroms_up"])
 
-    def test_update_logic(self):
+class TestSnpTkAppUpdateLogic(unittest.TestCase):
+
+    def test_snp_up(self):
+        snp_map = [('rs123', '6:123', 'rs456')]
+        dbsnp = {'rs456': '6:123'}
+
+        expected = UpdateLogicOutput(
+                snps_del=[], snps_up=[('rs123', 'rs456')], coords_up=[], chroms_up=[])
+
+        self.assertEqual(snptk.app.update_logic(snp_map, dbsnp), expected)
+
+    def test_snp_up_chrom_up(self):
         snp_map = [('rs123', '6:123', 'rs456')]
         dbsnp = {'rs456': '7:123'}
 
-        snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update = [], [('rs123', 'rs456')], [], [('rs456', '7')]
+        expected = UpdateLogicOutput(
+                snps_del=[], snps_up=[('rs123', 'rs456')], coords_up=[], chroms_up=[('rs456', '7')])
 
-        result = snptk.app.update_logic(snp_map, dbsnp)
+        self.assertEqual(snptk.app.update_logic(snp_map, dbsnp), expected)
 
-        self.assertEqual(result, (snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update))
-
-    # no merge history and not in dbsnp
-    def test_update_logic_2(self):
+    def test_no_merge_no_dbsnp(self):
         snp_map = [('rs123', '6:123', 'rs123')]
         dbsnp = {}
 
-        snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update = ['rs123'], [], [], []
+        expected = UpdateLogicOutput(
+                snps_del=['rs123'], snps_up=[], coords_up=[], chroms_up=[])
 
-        result = snptk.app.update_logic(snp_map, dbsnp)
+        self.assertEqual(snptk.app.update_logic(snp_map, dbsnp), expected)
 
-        self.assertEqual(result, (snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update))
-
-    def test_update_logic_no_merge_history_but_in_dbsnp(self):
+    def test_no_merge_history_but_in_dbsnp(self):
         snp_map = [('rs123', '6:123', 'rs123')]
         dbsnp = {'rs123': '7:456'}
 
-        snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update = [], [], [('rs123', '456')], [('rs123', '7')]
+        expected = UpdateLogicOutput(
+                snps_del=[], snps_up=[], coords_up=[('rs123', '456')], chroms_up=[('rs123', '7')])
 
-        result = snptk.app.update_logic(snp_map, dbsnp)
+        self.assertEqual(snptk.app.update_logic(snp_map, dbsnp), expected)
 
-        self.assertEqual(result, (snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update))
+    def test_snp_up_but_up_snp_already_present(self):
+        snp_map = [('rs123', '6:123', 'rs456'),
+                   ('rs456', '6:123', 'rs456')]
 
-    # if snp_id_new in [snp[0] for snp in snp_map]:
-    def test_update_logic_4(self):
-        snp_map = [('rs123', '6:123', 'rs456'), ('rs456', '6:123', 'rs456')]
-        dbsnp = {'rs123': '7:456', 'rs456': '6:123'}
+        dbsnp = {'rs123': '7:456',
+                 'rs456': '6:123'}
 
-        snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update = ['rs123'], [], [], []
+        expected = UpdateLogicOutput(
+                snps_del=['rs123'], snps_up=[], coords_up=[], chroms_up=[])
 
-        result = snptk.app.update_logic(snp_map, dbsnp)
+        self.assertEqual(snptk.app.update_logic(snp_map, dbsnp), expected)
 
-        self.assertEqual(result, (snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update))
+    def test_snp_merged_but_merged_was_already_present_and_update_position(self):
+        snp_map = [('rs123', '6:123', 'rs456'),
+                   ('rs456', '6:123', 'rs456')]
 
-    def test_update_logic_when_snp_merged_but_merged_was_already_present_and_update_position(self):
-        snp_map = [('rs123', '6:123', 'rs456'), ('rs456', '6:123', 'rs456')]
-        dbsnp = {'rs123': '7:456', 'rs456': '6:1000'}
+        dbsnp = {'rs123': '7:456',
+                 'rs456': '6:1000'}
 
-        snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update = ['rs123'], [], [('rs456', '1000')], []
+        expected = UpdateLogicOutput(
+                snps_del=['rs123'], snps_up=[], coords_up=[('rs456', '1000')], chroms_up=[])
 
-        result = snptk.app.update_logic(snp_map, dbsnp)
-
-        self.assertEqual(result, (snps_to_delete, snps_to_update, coords_to_update, chromosomes_to_update))
+        self.assertEqual(snptk.app.update_logic(snp_map, dbsnp), expected)
